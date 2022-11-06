295a1b7a99d65c30eed4fb82fbd1c5c6eb32dc29
nicolas-gagnon <nicolas.gagnon4@hotmail.com>
2022-11-06 14:45:36 -0500

    ioc poc

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
     <div className="App">
       <div className="mt-5 row" style={{"background-color": "black", "align-items": "center", "justify-content": "center"}}>
         <img src={logo} style={{"height": "5vmin"}} alt="logo"></img>
-        <h1 style={{"color": "white"}}>IoT Messaging</h1>
+        <h1 style={{"color": "white"}}>IoT POC </h1>
         <a href="https://www.etsmtl.ca/" className="pl-4"><img src="https://www.etsmtl.ca/content/img/logo_ets.svg" width="400" 
      height="300"  alt="Powered by ETS Cloud Computing"></img></a>
       </div>
