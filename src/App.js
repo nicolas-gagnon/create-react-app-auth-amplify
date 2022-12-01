181f2dc5829f974d3a8d97996cff0c79fcab4851
nicolas-gagnon <nicolas.gagnon4@hotmail.com>
2022-12-01 18:39:29 -0500

    update

--- src/App.js
+++ src/App.js
@@ -45,8 +45,8 @@ function App() {
       <div className="mt-5 row" style={{"background-color": "black", "align-items": "center", "justify-content": "center"}}>
         <img src={logo} style={{"height": "5vmin"}} alt="logo"></img>
         <h1 style={{"color": "white"}}>IoT POC </h1>
-        <a href="https://www.etsmtl.ca/" className="pl-4"><img src="https://www.etsmtl.ca/content/img/logo_ets.svg" width="100" 
-     height="300"  alt="Powered by ETS The  Computing"></img></a>
+        <a href="https://www.etsmtl.ca/" className="pl-4"><img src="https://www.etsmtl.ca/content/img/logo_ets.svg" width="400" 
+     height="300"  alt="Powered by ETS The Cloud Computing"></img></a>
       </div>
       <div className="row">
         <div id="publisher" className="col ml-5 mt-5 mb-5 mr-3" style={{"border-style": "solid", "border-width": "2px"}}>
